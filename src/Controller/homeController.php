90c7ee0df0e97e3f465fe2b1f8ded9db4ae95c90
cristian <caafanadord@ufpso.edu.co>
2021-12-29 15:49:23 -0500

    modificación error

---
+++ src/Controller/homeController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use App\Resource\Security\Encrypt;
+use App\Service\spotifyService;
+
+class homeController extends AbstractController {
+
+
+    /**
+     * @Route("/", name="index")
+     * @param Request $request
+     * @return Response
+     */
+    public function index() {
+        return $this->redirectToRoute('lanzamientos');
+
+    }
+
+    /**
+     * @Route("/lanzamientos", name="lanzamientos")
+     * @param Request $request
+     * @return Response
+     */
+    public function lanzamientos(Request $request, spotifyService $spotify) {
+        $this->verificarToken($spotify ,$request);
+        $data = $spotify->getNewAlbum();
+        return $this->render('home.html.twig', [
+            'title' => 'lanzamientos',
+            'datas' => count($data) > 0  ?  $data["albums"]["items"] : []
+        ]);
+    }
+
+    /**
+     * @Route("/artista/{id}", name="artista", requirements={"id"=".+"})
+     * @param Request $request
+     * @return Response
+     */
+    public function artista(String $id, Request $request, spotifyService $spotify){
+        $this->verificarToken($spotify ,$request);  
+        $artista = $spotify->getArstist($id);
+        $track = $spotify->getTopTrackArtist($id);
+        return $this->render('artista.html.twig', [
+            'title' => 'artista',
+            'artista' => count($artista) > 0  ?  $artista : [],
+            'top' => count($track) > 0  ?  $track["tracks"] : []
+        ]);
+    }
+
+
+    private function verificarToken($spotify , $request){
+        $cookie = $request->cookies->get("SAPISID");
+        if(!$cookie){
+           $cookie= $this->generateCookie($spotify->getToken());
+        }
+        $spotify->setToken($cookie);
+    }
+
+    private function generateCookie($token){
+        $cookie = Cookie::create('SAPISID')
+                ->withValue($token["token"])
+                ->withExpires(time() +  $token["time"])
+                ->withDomain('localhost')
+                ->withSecure(true)
+                ->WithHttpOnly(true);
+        $response = new Response();
+        $response->headers->setCookie($cookie);
+        $response->send();
+        return $token["token"];
+    }
+}
---
+++ src/Service/spotifyService.php
@@ -0,0 +1,79 @@
+<?php
+namespace App\Service;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class spotifyService{
+
+    private $client;
+    private $token ;
+    private $time;
+
+
+    public function __construct(HttpClientInterface $client){
+        $this->client = $client;
+    }
+
+    private function connectionAuth (){
+        $response = $this->client->request('POST','https://accounts.spotify.com/api/token',
+            [ 
+                'headers' => [
+                    'Content-Type' => 'application/x-www-form-urlencoded',
+                    'Authorization' => 'Basic '. base64_encode($_ENV["ID_CLIENTE"]. ':'. $_ENV["CLIENT_SECRET"]),
+                ],
+                'body' => ['grant_type' => 'client_credentials']
+        ]);
+        $statusCode = $response->getStatusCode();
+        $contentType = $response->getHeaders()['content-type'][0];
+        $content = $response->toArray();
+        $this->token=  $content["token_type"].' '.$content["access_token"];
+        $this->time=  $content["expires_in"];
+        
+       return ['token' => $this->token, 'time' => $this->time ];
+    }
+
+    public function getToken(){
+        return $this->connectionAuth();
+    }
+
+    public function setToken($token){
+        $this->token = $token; 
+    }
+
+
+    private function connectionSearch( $url , $param = '', $method = "GET"   ){
+        $id =  ($param) ? "/$param" : '' ;
+        $response = $this->client->request($method , "https://api.spotify.com/v1/$url".$id ,
+            [
+                'headers' =>[
+                    'Content-Type' => 'application/json',
+                    'Authorization' => $this->token
+                ]
+            ] );
+        $statusCode = $response->getStatusCode();
+        $contentType = $response->getHeaders()['content-type'][0];
+        $content = $response->toArray();
+       return $content;
+    }
+
+    public function getNewTrack($id = ''){
+        $param = '?q=tag:new&type=album';
+        return $this->connectionSearch('search' . $param );	
+    }
+
+    public function getNewAlbum($id = ''){
+        return $this->connectionSearch('browse' ,'new-releases');	
+    }
+
+    public function getArstist($id = ''){
+       return $this->connectionSearch('artists' ,$id);
+    }
+
+    public function getAlbumArtist($id = ''){
+       return $this->connectionSearch('artists' ,$id .'/albums?market=ES');
+    }
+
+    public function getTopTrackArtist($id = ''){
+        return $this->connectionSearch('artists' ,$id .'/top-tracks?market=ES');
+     }
+}
